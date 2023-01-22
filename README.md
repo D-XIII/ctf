3131129ec1c8f9383012c41c9856814325a62bab
D-XIII <hugo.benard@live.fr>
2023-01-22 05:48:36 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CTF - DXIII  
+# CTF - DX-III  
+
+Challenge created for the Pole Sup Delasalle by DX-III  
+
+## Start the challenge  
+
+In order to start the challege juste go to the root directory and use  
+`docker-compose up`  
+It will create 2 container:  
+- the app container  
+- the database container  
--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6be0c235cafea70f"; }
+/******/ 		__webpack_require__.h = function() { return "0bf1a8010441458f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
